931ec2af7ffaa66609c0f564ab3729d9807ce7b0
wedgevine <wedgevine@outlook.com>
2022-07-13 21:33:44 -0400

    Exercises 1.3.-1.5.

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,55 +1,55 @@
 const Header = (props) => {
     return (
-        <h1>{props.course}</h1>
+        <h1>{props.course.name}</h1>
     )
 }
 
 const Part = (props) => {
-    //console.log(props)
     return (
-        <p> {props.partNumber} {props.exerciseCount} </p>
+        <p> {props.partName} {props.exerciseCount} </p>
     )
 }
 
 const Content = (props) => {
     return (
         props.parts.map((part, index) => {
-            //console.log(index, part)
-            return <Part key={index} partNumber={part.partNumber} exerciseCount={part.exerciseCount} />
-            /*
-            return (
-                <p> {part.partNumber} {part.exerciseCount} </p>
-            )
-            */
+            return <Part key={index} partName={part.name} exerciseCount={part.exercises} />
         })
     )
 }
 
 const Total = (props) => {
+    let total = 0
+    props.parts.map(part => total += part.exercises)
     return (
-        <p>Number of exercises {props.total}</p>
+        <p>Number of exercises {total}</p>
     )
 }
 
 const App = () => {
-    const course = 'Half Stack application development'
-    const part1 = 'Fundamentals of React'
-    const exercises1 = 10
-    const part2 = 'Using props to pass data'
-    const exercises2 = 7
-    const part3 = 'State of a component'
-    const exercises3 = 14
+    const course = {
+        name: 'Half Stack application development',
+        parts: [
+            {
+                name: 'Fundamentals of React',
+                exercises: 10
+            },
+            {
+                name: 'Using props to pass data',
+                exercises: 7
+            },
+            {
+                name: 'State of a component',
+                exercises: 14
+            }
+        ]
+    }
 
     return (
         <>
             <Header course={course} />
-            <Content parts={[
-                {partNumber: part1, exerciseCount: exercises1},
-                {partNumber: part2, exerciseCount: exercises2},
-                {partNumber: part3, exerciseCount: exercises3},
-                ]}
-            />
-            <Total total={exercises1 + exercises2 + exercises3} />
+            <Content parts={course.parts} />
+            <Total parts={course.parts} />
         </>
     )
 }
