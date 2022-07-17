f61467a582f90aa89273cd84d5ace15d33550679
wedgevine <wedgevine@outlook.com>
2022-07-17 16:18:19 -0400

    Exercise 2.11 phonebook

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,19 +1,31 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 import Search from './components/Search'
 import AddPerson from './components/AddPerson'
 import Persons from './components/Persons'
 
 const App = () => {
+    const [persons, setPersons] = useState([])
+    /*
     const [persons, setPersons] = useState([
         { name: 'Arto Hellas', number: '040-123456', id: 1 },
         { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
         { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
         { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
     ])
+    */
     const [newName, setNewName] = useState('')
     const [newNumber, setNewNumber] = useState('')
     const [searchName, setSearchName] = useState('')
+
+    useEffect(() => {
+        axios
+            .get('http://localhost:3002/persons')
+            .then(response => {
+                setPersons(response.data)
+            })
+    }, [])
 
     const filteredPersons = persons.filter(
         (person) => person.name.toLowerCase().includes(searchName.toLowerCase())
